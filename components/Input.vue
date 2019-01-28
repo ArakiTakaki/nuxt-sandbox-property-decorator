cac513c59778c815d53e3ff611a141f0a3ef148b
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-01-29 01:05:49 +0900

    initialcommit

---
+++ components/Input.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.root
+  input(type="text" :value="$props.value" @input="onInput")
+</template>
+
+<script>
+import Vue from "vue";
+import { Component, Prop, Model, Emit } from "vue-property-decorator";
+
+@Component({
+  props: {
+    value: String
+  },
+  model: {
+    prop: 'value',
+    event: 'input'
+  }
+})
+class Input extends Vue {
+  @Emit('input')
+  onInput(event) {
+    return event.target.value;
+  }
+}
+
+export default Input;
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <p>{{ count }}</p>
+    <button @click='countUp'>count up</button>
+    <Input v-model="$data.sample" />
+    {{ $data.sample }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+
+// Components
+import Input from '../components/Input.vue';
+
+@Component({
+  components: {Input}
+})
+export default class Index extends Vue {
+  count = 3
+  sample = 'test'
+  countUp () {
+    this.count++
+  }
+  input(value) {
+    console.log(value)
+  }
+}
+</script>
